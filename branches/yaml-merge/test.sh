88de350dc6a387a31b738db7edec665a476f790a
bpd@keynetics.com <bpd@keynetics.com@f187079a-d9f3-11de-a9c8-216ecde6309e>
2009-11-30 01:12:12 +0000

    Branching to develop merge feature

---
+++ branches/yaml-merge/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Run phpt tests
+#
+
+TEST=${1:-tests/}
+
+TEST_PHP_EXECUTABLE=/usr/bin/php \
+/usr/bin/php \
+  -n -d open_basedir= -d output_buffering=0 -d memory_limit=-1 \
+  run-tests.php -n -d extension_dir=modules/ -d extension=yaml.so ${TEST}
+  #-d extension_dir=modules/ -d extension=yaml.so \
